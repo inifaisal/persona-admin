a2825f32f42d84d2ff3627c8a111ff48aab9f263
Faisal <inifaisal.id@gmail.com>
2025-01-02 19:40:31 +0800

    wip: add dashboard components

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { AppSidebar } from '../components/blocks/app-sidebar'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '../components/ui/sidebar'
-import { Separator } from '@/components/ui/separator'
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { SidebarInset, SidebarProvider } from '../components/ui/sidebar'
 
 export const Route = createRootRoute({
   component: () => (
@@ -11,28 +9,9 @@ export const Route = createRootRoute({
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Home
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
           <Outlet />
-          <TanStackRouterDevtools />
         </SidebarInset>
+        <TanStackRouterDevtools />
       </SidebarProvider>
     </>
   ),
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,3 +1,9 @@
+import { OverviewChart } from '@/components/blocks/chart/overview-chart'
+import { SampleChart } from '@/components/blocks/chart/sample-chart'
+import { UsersTable } from '@/components/blocks/table/users-table'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import { SidebarTrigger } from '@/components/ui/sidebar'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/')({
@@ -6,8 +12,37 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <>
+    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <div className="flex items-center gap-2 px-4">
+      <SidebarTrigger className="-ml-1" />
+      <Separator orientation="vertical" className="mr-2 h-4" />
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem className="hidden md:block">
+            <BreadcrumbLink href="#">
+              Home
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="hidden md:block" />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="hidden md:block" />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Overview</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
     </div>
+  </header>
+    <div className="grid p-4 gap-4">
+      <OverviewChart />
+      <div className='grid gap-4 sm:grid-cols-3 lg:grid-cols-3'>
+        <UsersTable />
+        <SampleChart />
+      </div>
+    </div>
+    </>
   )
 }
